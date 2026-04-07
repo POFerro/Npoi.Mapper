48190a4d7b61d6f4a5242c0e00f17937833128f4
Paulo Ferro <NB12012@novabase.pt>
2026-04-07 11:22:39 +0100

    Upgrade nuget dependencies, support .net 8 and 10

--- Npoi.Mapper/src/Npoi.Mapper/MapHelper.cs
+++ Npoi.Mapper/src/Npoi.Mapper/MapHelper.cs
@@ -391,7 +391,7 @@ public class MapHelper
                 break;
 
             case CellType.Error:
-            case CellType.Unknown:
+            case CellType._None:
             case CellType.Blank:
                 // Dose nothing to keep return value null.
                 break;
